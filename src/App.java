681ad0cf14f8a5eb882baf35dcca1c918cd33bdd
apipi <ikucastro23@lhusurbil.eus>
2024-01-16 12:07:52 +0100

    commit adios

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception 
     {
         System.out.println("Hello, World!");
-        System.out.println(5000);
+        System.out.println("adios");
     }
 }
